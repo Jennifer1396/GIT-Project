1810a90ce88a17dc140d10d9d7e5994f480f4b1f
Jennifer1396 <jenijoe_j@yahoo.com>
2025-05-25 21:10:16 +0530

    Jeni code completed

---
+++ src/Git_Pull.java
@@ -0,0 +1,14 @@
+
+public class Git_Pull {
+
+	private void gitpull() {
+		System.out.println("Pulled request");
+
+	}
+	
+	private void gitpush() {
+		System.out.println("Pushed request");
+
+	}
+
+}
